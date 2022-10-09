44a9335e9517bce7d10e99920e880408864047bc
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-10-09 09:39:55 -0400

    fixed syntax errors with paths

--- my-app/src/components/Resume.js
+++ my-app/src/components/Resume.js
@@ -1,23 +1,56 @@
 import React from "react";
 
 function Resume() {
-    return (
-        <div>
-            <div><h2>Resume</h2></div>
-            <div>
-                <p className="content is-medium">Skills</p>
-                <hr />
-                <ul>
-                    <li>HTML, CSS, Javascript</li>
-                    <li>OpenAPI, JSON</li>
-                    <li>React</li>
-                    <li>Markdown</li>
-                    <li>Git</li>
-                    <li>Github Repo Management</li>
-                </ul>
-            </div>
-        </div>
-    );
+	return (
+		<section>
+			<div className="center">
+				<h1 className="page-header">My Resume</h1>
+			</div>
+			<div className="bottom-spacing">
+				<a href={require("../assets/Michael Su Resume.pdf")} download>
+					<h4>Download my Resume</h4>
+				</a>
+			</div>
+			<div>
+				<h5>Front-End Proficiencies</h5>
+				<ol>
+					<li>HTML5</li>
+					<li>CSS3</li>
+					<li>JavaScript</li>
+					<li>jQuery</li>
+					<li>Bootstrap</li>
+					<li>React</li>
+					<li>version control/Git</li> 
+					<li>responsive design</li>
+				</ol>
+				<br></br>
+				<h5>Back-End Proficiencies</h5>
+				<ol>
+					<li>APIs</li>
+					<li>NodeJS</li>
+					<li>Express</li>
+					<li>Model View Controller (MVC)</li>
+					<li>REST</li>
+				</ol>
+				<br></br>
+				<h5>Dev Tool Proficiencies</h5>
+				<ol>
+					<li>Git</li>
+					<li>npm</li>
+					<li>Jest</li>
+				</ol>
+				<br></br>
+				<h5>Database Proficiencies</h5>
+				<ol>
+					<li>MySQL</li>
+					<li>Sequelize</li>
+					<li>NoSQL</li>
+					<li>MongoDB</li>
+					<li>Mongoose</li>
+				</ol>
+			</div>
+		</section>
+	);
 }
 
 export default Resume;
